41a2039eeef21f14269fed6e8a4a51ff956b38aa
vitalii kravets <vkravets@amazon.com>
2017-08-25 22:18:59 +0200

    Initial commit

---
+++ presentation/index.js
@@ -0,0 +1,140 @@
+// Import React
+import React from "react";
+
+// Import Spectacle Core tags
+import {
+  BlockQuote,
+  Cite,
+  Deck,
+  Fill,
+  Fit,
+  Heading,
+  Image,
+  Layout,
+  Link,
+  ListItem,
+  List,
+  Quote,
+  Slide,
+  Text
+} from "spectacle";
+
+// Import image preloader util
+import preloader from "spectacle/lib/utils/preloader";
+
+// Import theme
+// import createTheme from "spectacle/lib/themes/default";
+import theme from "../themes/seattlejs/index.js";
+
+// Require CSS
+require("normalize.css");
+require("spectacle/lib/themes/default/index.css");
+require("../themes/seattlejs/index.css");
+// Best way to include fonts rite
+require("../fonts/worksans.css");
+require("../fonts/biorhyme.css");
+require("../fonts/silkscreen.css");
+
+const images = {
+  bg: require("../assets/bg_berlin.svg"),
+  badCode: require("../assets/bad_code.jpg"),
+  sonarqube: require("../assets/sonarqube.svg"),
+  sonarqubeReport: require("../assets/sonarqube-report.png"),
+  soWhat: require("../assets/so-what.gif")
+};
+
+preloader(images);
+
+export default class Presentation extends React.Component {
+  renderSponsorHeading(text) {
+    return (
+      <Heading
+        caps
+        size={6}
+        style={{ letterSpacing: "0.05em" }}
+      >
+        {text}
+      </Heading>
+    );
+  }
+
+  render() {
+    const skylineBg = {
+      backgroundImage: `url(${images.bg})`,
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "bottom center",
+      backgroundSize: "100% auto"
+    };
+
+    const sponsor = {
+      display: "block",
+      marginBottom: "1.5em",
+      maxHeight: "600px"
+    };
+
+    return (
+      <Deck
+        progress="none"
+        theme={theme}
+        transition={["fade"]}
+        transitionDuration={500}
+      >
+        <Slide style={skylineBg}>
+          <Heading size={4}>Thing I learned in Berlin</Heading>
+          <Text margin="auto auto 5em auto">
+            @ WebDevCon 2-3 August 2017
+          </Text>
+        </Slide>
+        <Slide bgColor="white">
+          <Heading
+            caps
+            size={4}
+            style={{ letterSpacing: "0.05em" }}
+          >
+            How good is our code?
+          </Heading>
+          <br/><br/>
+          <Image width="100%" src={images.badCode} />
+        </Slide>
+        <Slide bgColor="white">
+          <Image style={sponsor} width="100%" src={images.sonarqube} />
+        </Slide>
+        <Slide bgColor="white">
+          <Image style={sponsor} width="100%" src={images.sonarqubeReport} />
+        </Slide>
+        <Slide bgColor="black">
+          <Image width="100%" src={images.soWhat} />
+            <Heading
+              caps
+              size={4}
+              style={{ letterSpacing: "0.05em", color: "white" }}
+            >
+              So what?!
+            </Heading>
+        </Slide>
+        <Slide bgColor="white">
+          <Heading
+            caps
+            size={5}
+            style={{ letterSpacing: "0.05em" }}
+          >
+            The cost of bad code:
+          </Heading>
+          <br/><br/>
+          <Text margin="auto auto 6em auto" textSize="3rem">
+            More bugs
+          </Text>
+          <Text margin="auto auto 6em auto" textSize="3rem">
+            Hard to extend
+          </Text>
+          <Text margin="auto auto 6em auto" textSize="3rem">
+            Depression
+          </Text>
+        </Slide>
+        <Slide>
+
+        </Slide>
+      </Deck>
+    );
+  }
+}
